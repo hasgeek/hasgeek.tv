ad865c191bdca1c4d152bf6c0ff1c42d66933911
Nigel Babu <nigel@hasgeek.com>
2012-04-12 16:57:57 +0530

    Redo the work from yesterday to place an add to playlist form on video page

--- hgtv/forms/playlist.py
+++ hgtv/forms/playlist.py
@@ -3,8 +3,11 @@
 from baseframe.forms import Form
 import flask.ext.wtf as wtf
 
-__all__ = ['PlaylistForm']
+__all__ = ['PlaylistForm', 'PlaylistAddForm']
 
 
 class PlaylistForm(Form):
     title = wtf.TextField('Title', validators=[wtf.Required()])
+
+class PlaylistAddForm(Form):
+    playlist = wtf.SelectField('Add to playlist', coerce=int)
--- hgtv/views/video.py
+++ hgtv/views/video.py
@@ -5,8 +5,8 @@ from coaster.views import load_model
 from baseframe.forms import render_form, render_redirect
 
 from hgtv import app
-from hgtv.forms import VideoAddForm
-from hgtv.models import Channel, Video, db
+from hgtv.forms import VideoAddForm, PlaylistAddForm
+from hgtv.models import Channel, Video, Playlist, db
 from hgtv.views.login import lastuser
 
 from urlparse import urlparse, parse_qs
@@ -18,19 +18,32 @@ def video_namespace(channel):
     return redirect(url_for('channel', channel=channel.name))
 
 
-@app.route('/<channel>/view/<video>')
+@app.route('/<channel>/view/<video>', methods=['GET', 'POST'])
 @load_model([
     (Channel, {'name': 'channel'}, 'channel'),
     (Video, {'name': 'video', 'channel': 'channel'}, 'video')
     ])
 def video_view(channel, video):
+    form = PlaylistAddForm()
+    playlists = set(channel.playlists) - set(video.playlists)
+    form.playlist.choices = [(p.id, p.title) for p in playlists]
+    if form.validate_on_submit():
+        if channel.userid != g.user.userid:
+            if channel.userid not in [org['userid'] for org in g.lastuserinfo.organizations['owner']]:
+                abort(403)
+        playlist = Playlist.query.get(form.data['playlist'])
+        video.playlists.append(playlist)
+        db.session.add(video)
+        db.session.commit()
+        flash(u"Added video '%s'." % video.title, 'success')
     video_id = parse_qs(urlparse(video.url).query)['v'][0]
-    return render_template('video.html', title=video.title, channel=channel, video=video, video_id=video_id)
+    return render_template('video.html', title=video.title, channel=channel, video=video, video_id=video_id, form=form)
 
 @app.route('/<channel>/video/new', methods=['GET', 'POST'])
 @load_model(Channel, {'name': 'channel'}, 'channel')
-@lastuser.requires_login
 def video_add(channel):
+    if not g.user:
+        abort(403)
     if channel.userid != g.user.userid:
         if channel.userid not in [org['userid'] for org in g.lastuserinfo.organizations['owner']]:
             abort(403)
