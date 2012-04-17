ec281c6c4a04828b61609b98d3523726f6b6576a
Nigel Babu <nigel@hasgeek.com>
2012-04-17 13:12:41 +0530

    Form for editing video

--- hgtv/forms/video.py
+++ hgtv/forms/video.py
@@ -3,7 +3,13 @@
 from baseframe.forms import Form
 import flask.ext.wtf as wtf
 
-__all__ = ['VideoAddForm']
+__all__ = ['VideoAddForm', 'VideoEditForm']
 
 class VideoAddForm(Form):
     url = wtf.TextField('Video URL', validators=[wtf.Required()])
+
+
+class VideoEditForm(Form):
+    description = wtf.TextAreaField('Description')
+    url = wtf.TextField('Video URL', validators=[wtf.Required()]
+    slides = wtf.TextField('Slides URL')
