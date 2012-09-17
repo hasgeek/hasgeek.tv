51d9837c43156150af24f2115ba378d8088138fc
kracekumar <kracethekingmaker@gmail.com>
2012-09-17 18:28:10 +0530

    Changes for add to playlist & new modal window

--- hgtv/views/channel.py
+++ hgtv/views/channel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from flask import render_template, g, flash, jsonify, request, abort, escape
-from coaster.views import load_model
+from coaster.views import load_model, load_models
 from baseframe.forms import render_redirect, render_form
 
 from hgtv import app
@@ -53,19 +53,20 @@ def user_playlists(video):
         return html
 
 
-@app.route('/<channel>/action', methods=['POST'])
+@app.route('/<channel>/<playlist>/add', methods=['POST'])
 @lastuser.requires_login
-@load_model(Channel, {'name': 'channel'}, 'channel', permission='new-playlist')
-def channel_action(channel):
+@load_models(
+    (Channel, {'name': 'channel'}, 'channel'),
+    (Playlist, {'name': 'playlist', 'channel': 'channel'}, 'playlist'),
+    permission='new-video')
+def channel_action(channel, playlist):
     """
     Perform an action in the channel
     """
     form = VideoCsrfForm()
-    video_name = unicode(escape(request.form.get('video_name')))
-    playlist_name = unicode(escape(request.form.get('playlist_name')))
-    if video_name and playlist_name and form.validate():
+    video_name = unicode(escape(request.args.get('video')))
+    if video_name and form.validate():
         video = Video.query.filter_by(name=video_name).first()
-        playlist = Playlist.query.filter_by(name=playlist_name).first()
         if video:
             # if owner deletes the video while other user is viewing the video
             if video in playlist.videos:
@@ -78,34 +79,42 @@ def channel_action(channel):
                     action = u'info'
                     message = u'Cannot remove video from primary playlist'
                     message_type = u'info'
-                to_return = {'message_type': message_type, 'action': action, 'message': message, 'playlist_name': playlist_name}
+                to_return = {'message_type': message_type, 'action': action, 'message': message, 'playlist_name': playlist.name}
             else:
                 playlist.videos.append(video)
                 to_return = {'message_type': 'success', 'action': 'add',
-                    'message': u"Video successfully added to playlist", 'playlist_name': playlist_name}
+                    'message': u"Video successfully added to playlist", 'playlist_name': playlist.name}
         else:
             to_return = {'message_type': 'error', 'action': 'add',
-                    'message': u"Video doesn't exist", 'playlist_name': playlist_name}
+                    'message': u"Video doesn't exist", 'playlist_name': playlist.name}
         db.session.commit()
         return jsonify(to_return)
     abort(403)
 
 
-@app.route('/<channel>/new_playlist_ajax', methods=['POST'])
+@app.route('/<channel>/new_playlist_ajax/<video>', methods=['POST', 'GET'])
 @lastuser.requires_login
-@load_model(Channel, {'name': 'channel'}, 'channel', permission='new-playlist')
-def playlist_new_modal(channel):
+@load_models(
+    (Channel, {'name': 'channel'}, 'channel'),
+    (Video, {'url_name': 'video'}, 'video'), permission='new-playlist')
+def playlist_new_modal(channel, video):
     # Make a new playlist
     form = PlaylistForm()
     form.channel = channel
-    if form.validate_on_submit():
-        playlist = Playlist(channel=channel)
-        form.populate_obj(playlist)
-        if not playlist.name:
-            playlist.make_name()
-        db.session.add(playlist)
-        db.session.commit()
-        html_to_return = "<li><a href='#' id='playlist-item' data='" + playlist.name + "'>" + playlist.title
-        return jsonify({'html': html_to_return, 'message_type': 'success', 'action': 'append',
-          'message': '%s playlist created' % (playlist.name)})
-    return jsonify({'message_type': "error", 'action': 'append', 'html': map(lambda x: unicode(x) + u'_error', form.errors.keys())})
+    html = render_template('playlist-modal.html', form=form, channel=channel, video=video)
+    if request.is_xhr:
+        if form.validate_on_submit():
+            playlist = Playlist(channel=channel)
+            form.populate_obj(playlist)
+            if not playlist.name:
+                playlist.make_name()
+            db.session.add(playlist)
+            db.session.commit()
+            html_to_return = render_template('new-playlist-tag.html', playlist=playlist, channel=channel, video=video)
+            return jsonify({'html': html_to_return, 'message_type': 'success', 'action': 'append',
+                'message': '%s playlist created' % (playlist.name)})
+        if form.errors:
+            return jsonify({'message_type': "error", 'action': 'append',
+                'html': map(lambda x: unicode(x) + u'_error', form.errors.keys())})
+        return jsonify(html=html, message_type='success', action='modal-window')
+    return html
