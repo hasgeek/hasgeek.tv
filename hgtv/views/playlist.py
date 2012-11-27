26261e0292dd95b3ebfbe8b1a575eb1144b57d28
kracekumar <me@kracekumar.com>
2012-11-27 18:05:48 +0530

    Made any logged in user to upload video

--- hgtv/views/playlist.py
+++ hgtv/views/playlist.py
@@ -93,8 +93,6 @@ def process_playlist(playlist, playlist_url):
 @load_model(Channel, {'name': 'channel'}, 'channel', permission='new-playlist')
 def playlist_new(channel):
     # Make a new playlist
-    if not lastuser.has_permission('siteadmin'):
-        abort(403)
     form = PlaylistForm()
     form.channel = channel
     if form.validate_on_submit():
@@ -156,8 +154,6 @@ def playlist_view(channel, playlist):
 @load_model(Channel, {'name': 'channel'}, 'channel', permission='new-playlist')
 def playlist_import(channel):
     # Import playlist
-    if not lastuser.has_permission('siteadmin'):
-        abort(403)
     form = PlaylistImportForm()
     form.channel = channel
     if form.validate_on_submit():
