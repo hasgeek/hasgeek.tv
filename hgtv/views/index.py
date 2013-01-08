7d31ae404c75153929d76460cd55c3faf84099fa
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-01-08 18:23:30 +0530

    Humans.txt and removed favicon route.

--- hgtv/views/index.py
+++ hgtv/views/index.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-from flask import send_from_directory, render_template
+from flask import render_template
 from baseframe.forms import render_message
 from hgtv import app
 from hgtv.models import Playlist
@@ -23,12 +22,6 @@ def longdate(date):
 def index():
     playlists = Playlist.get_featured(10)
     return render_template('index.html', playlists=playlists)
-
-
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static', 'img'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 
 @app.route('/search')
