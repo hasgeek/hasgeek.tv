fb6350a7d4c04a3862ee6d58890db0184ccad2df
kracekumar <me@kracekumar.com>
2013-02-17 22:03:25 +0530

    tracked sample image

--- runserver.py
+++ runserver.py
@@ -4,4 +4,4 @@ from hgtv import app, init_for
 from hgtv.models import db
 init_for('development')
 db.create_all()
-app.run('0.0.0.0', debug=True, port=8001)
+app.run('0.0.0.0', debug=True, port=8000)
