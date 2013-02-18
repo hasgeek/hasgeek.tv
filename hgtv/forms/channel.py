3d67dd0c234fff9c705c7d0992492886302980cb
Kiran Jonnalagadda <kiran@hasgeek.com>
2013-02-18 05:30:36 +0530

    Channel logo message.

--- hgtv/forms/channel.py
+++ hgtv/forms/channel.py
@@ -9,5 +9,5 @@ __all__ = ['ChannelForm']
 class ChannelForm(Form):
     type = wtf.SelectField(u"Channel type", coerce=int, validators=[wtf.Required()])
     description = RichTextField(u"Description")
-    channel_logo = wtf.FileField(u"Channel logo", description="Optional - Channel logo will appear in channel page, 320 * 240 px")
-    delete_logo = wtf.BooleanField(u"Check to remove logo", description="Remove existing logo?")
+    channel_logo = wtf.FileField(u"Channel logo", description="Optional - Channel logos are shown on the homepage when the channel is featured")
+    delete_logo = wtf.BooleanField(u"Remove existing logo?")
