32406479c844023b788fb264b64c80a8ff052499
kracekumar <me@kracekumar.com>
2013-04-13 11:17:33 +0530

    quoted all urls

--- hgtv/views/video.py
+++ hgtv/views/video.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+import urllib
 from urlparse import urlparse, parse_qs
 from socket import gaierror
 import requests
@@ -103,7 +104,7 @@ def process_slides(video):
 
 
 def get_slideshare_unique_value(url):
-    r = requests.get('http://www.slideshare.net/api/oembed/2?url=%s&format=json' % url)
+    r = requests.get('http://www.slideshare.net/api/oembed/2?url=%s&format=json' % urllib.quote(url))
     jsondata = r.json() if callable(r.json) else r.json
     return jsondata['slide_image_baseurl'].split('/')[-3]
 
@@ -112,7 +113,7 @@ def make_presentz_json(video, json_value):
     d = {"chapters": [{"video": {"url": video.video_url,}}]}
     if video.slides_source == u'slideshare':
         unique_value = get_slideshare_unique_value(video.slides_url)
-        d['chapters'][0]['slides'] = [{'time': str(key), "public_url": video.slides_url, "url": 'http://slideshare.net/' + unique_value + "#" + str(val)} for key, val in json_value.items()]        
+        d['chapters'][0]['slides'] = [{'time': str(key), "public_url": urllib.quote(video.slides_url), "url": 'http://slideshare.net/' + unique_value + "#" + str(val)} for key, val in json_value.items()]        
     elif video.slides_source == u'speakerdeck':
         #json to supply for presentz syncing
         d['chapters'][0]['slides'] = [{'time': str(key), "url": 'http://speakerdeck.com/' + video.slides_sourceid + "#" +str(val)} for key, val in json_value.items()]
