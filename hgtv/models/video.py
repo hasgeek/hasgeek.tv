cfdb41595eb0a5db163c58c5f64a57ad1d469ded
kracekumar <me@kracekumar.com>
2013-07-09 23:11:43 +0530

    Added featured video column

--- hgtv/models/video.py
+++ hgtv/models/video.py
@@ -38,6 +38,7 @@ class Video(BaseIdNameMixin, CommentingMixin, db.Model):
     slides_sourceid = db.Column(db.Unicode(80), nullable=False, default=u'')
     video_slides_mapping = db.Column(db.UnicodeText, nullable=True, default=u'')
     video_slides_mapping_json = db.Column(db.UnicodeText, nullable=True, default=u'')
+    featured = db.Column(db.Boolean, default=False, nullable=False)
 
     playlists = association_proxy('_playlists', 'playlist', creator=lambda x: PlaylistVideo(playlist=x))
 
