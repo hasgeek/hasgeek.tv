ac099b769ec830156ef0b616bdf2fbbd641cd9e9
kracekumar <me@kracekumar.com>
2013-08-07 14:00:48 +0530

    Update the channel name

--- hgtv/views/video.py
+++ hgtv/views/video.py
@@ -424,6 +424,7 @@ def video_add_speaker(channel, playlist, video):
         userinfo = lastuser.getuser_by_userid(speaker_buid)
         if userinfo:
             speaker_channel = Channel.query.filter_by(userid=userinfo['userid']).first()
+            print speaker_channel.__dict__,"Dict: ", userinfo
             if speaker_channel is None:
                 # Create a channel for this speaker. They have never logged in to hasgeek.tv
                 # at this point, but when they do, the channel will be waiting for them
