5c2e4c886afebd77629c35afd6e7f6f88a5dd128
kracekumar <me@kracekumar.com>
2013-09-06 11:20:23 +0530

    Added migration file

---
+++ alembic/versions/313a997eef03_removed_short_title_.py
@@ -0,0 +1,22 @@
+"""Removed short_title column
+
+Revision ID: 313a997eef03
+Revises: 1ed6e594b69c
+Create Date: 2013-09-06 11:13:48.325107
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '313a997eef03'
+down_revision = '1ed6e594b69c'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.drop_column('playlist', 'short_title')
+
+
+def downgrade():
+    op.add_column('playlist', sa.Column('short_title', sa.Unicode(80), nullable=False, server_default=sa.text(u"''")))
