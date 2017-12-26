77772ccaf11c4f2bcb565c9accd5061483981405
Vidya Ramakrishnan <vidya@hasgeek.in>
2017-12-26 12:30:42 +0530

    Remove unused config files.

--- hgtv/static/js/config/index.js
+++ hgtv/static/js/config/index.js
@@ -25,18 +25,4 @@ module.exports = {
     // Set to `true` or `false` to always turn it on or off
     bundleAnalyzerReport: process.env.npm_config_report
   },
-  dev: {
-    env: require('./dev.env'),
-    port: process.env.PORT || 8080,
-    autoOpenBrowser: true,
-    assetsSubDirectory: '',
-    assetsPublicPath: '/',
-    proxyTable: {},
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
-    cssSourceMap: false
-  }
 }
