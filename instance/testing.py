2c32ce716098422b8351b4d251e1735792d544a5
Bibhas <me@bibhas.in>
2018-08-20 16:42:06 +0530

    updated test config

--- instance/testing.py
+++ instance/testing.py
@@ -1,2 +1,8 @@
 #: Database backend
 SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+#: LastUser server
+LASTUSER_SERVER = 'https://auth.hasgeek.com/'
+#: LastUser client id
+LASTUSER_CLIENT_ID = ''
+#: LastUser client secret
+LASTUSER_CLIENT_SECRET = ''
