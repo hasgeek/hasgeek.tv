07b877823f6386a01550785cd723e6163202c0a4
Bibhas <me@bibhas.in>
2018-08-23 14:27:52 +0530

    fixed video url

--- hgtv/assets/src/components/Videos.vue
+++ hgtv/assets/src/components/Videos.vue
@@ -9,7 +9,7 @@
       <div v-else class="clearline"></div>
       <div class="grid playlist">
         <div v-if="videos" v-for="video in videos" class="grid__col-12 grid__col-xs-12 grid__col-sm-4 grid__col-lg-3 thumbnail-wrapper thumbnail-wrapper--margin">
-          <router-link :to="{ name: 'Video', params: { channel: channel.name, playlist: playlist.name, video:video.url }}" class="thumbnail thumbnail--video" v-if="video.thumbnail">
+          <router-link :to="{ name: 'Video', params: { channel: channel.name, playlist: playlist.name, video:video.url_name }}" class="thumbnail thumbnail--video" v-if="video.thumbnail">
             <img :src="video.thumbnail" class="img-responsive"/>
             <div class="overlay"></div>
             <i class="material-icons thumbnail__play-icon">play_circle_outline</i>
