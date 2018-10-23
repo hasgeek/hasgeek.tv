ae3c10a800a6de8ac1a729df2d791df46514316d
Vidya Ramakrishnan <vidya@hasgeek.in>
2018-10-23 22:36:49 +0530

    Change to dynamic tabs and not limit liverstreams to 2.

--- hgtv/assets/src/components/LiveStream.vue
+++ hgtv/assets/src/components/LiveStream.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="livestream grid__col-12">
-    <tabs v-if="livestreams[0]">
-      <tab name="Livestream 1">
+    <tabs v-if="livestreams">
+      <tab v-for="(stream, index) in livestreams" :key="index" :name="'Livestream ' + getstreamIndex(index)">
         <div class="video-box">
           <div class="video-box__video">
-            <iframe id="iframe-stream1" :src="livestreams[0]" frameborder="0" allowfullscreen></iframe>
-          </div>
-        </div>
-      </tab>
-      <tab name="Livestream 2" v-if="livestreams[1]">
-        <div class="video-box">
-          <div class="video-box__video">
-            <iframe id="iframe-stream1" :src="livestreams[1]" frameborder="0" allowfullscreen></iframe>
+            <iframe :id="'iframe-stream' + getstreamIndex(index)" :src="stream" frameborder="0" allowfullscreen></iframe>
           </div>
         </div>
       </tab>
@@ -23,19 +16,15 @@
 export default {
   name: 'LiveStream',
   props: ['livestreams'],
+  methods: {
+    getstreamIndex(index) {
+      return index + 1;
+    },
+  },
 };
 </script>
 
 <style scoped>
-  .livestream div {
-    width: 100%;
-  }
-  .tabs-component {
-    margin: 0 0 1em;
-  }
-  .tabs-component-panels {
-    padding: 2em 0;
-  }
   .video-box__video {
     padding-bottom: 56%;
     position: relative;
