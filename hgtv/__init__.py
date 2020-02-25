e99c74396d7d857701803171cbcf93644a554da8
Vidya Ramakrishnan <vidya@hasgeek.in>
2020-02-25 17:59:11 +0530

    Revert "Add material icon related dependency"
    This reverts commit 0049fefa59cf8385796342d0cc6a353c0cc26fd0.

--- hgtv/__init__.py
+++ hgtv/__init__.py
@@ -25,7 +25,7 @@ coaster.app.init_app(app)
 db.init_app(app)
 db.app = app
 migrate = Migrate(app, db)
-baseframe.init_app(app, requires=['baseframe-mui'], theme='mui', asset_modules=('baseframe_private_assets'))
+baseframe.init_app(app, requires=['baseframe-mui'], theme='mui')
 lastuser.init_app(app)
 lastuser.init_usermanager(UserManager(db, models.User))
 app.config['tz'] = timezone(app.config['TIMEZONE'])
